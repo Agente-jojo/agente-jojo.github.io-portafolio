ec9e1c795e7b313b215617de6adaa306b0d15f62
johsan <johsanmb@gmail.com>
2023-03-08 10:42:29 -0500

    Crearing response layout

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,13 @@
 const navigationHeight = document.querySelector('.head__navigation',).offsetHeight;
 document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 1 + 'px');
+
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.nav__list');
+const barras = document.querySelectorAll('.ham span');
+
+ham.addEventListener('click', () => {
+  enlaces.classList.toggle('activado');
+  barras.forEach(child => {child.classList.toggle('animado')});
+});
+
+
